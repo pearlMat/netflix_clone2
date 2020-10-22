3cfa69d9ef78b1d40f1e786039cffd5351896461
Pearl Takang <you@example.com>
2020-10-22 13:20:10 +0100

    added .eslintrc file and jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import Jumbotron from './components/jumbotron';
 
 export const App = () => (
-  
-    <div className="App">
-      <header className="App-header">
-     <div>Hello</div>
-      </header>
-    </div>
-  );
-
-
-
+	<div className="App">
+		<header className="App-header">
+			<Jumbotron />
+		</header>
+	</div>
+);
